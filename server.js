e9236f3bf2b1ae28e7d8a03997468633028d4b2a
kimdohyun <85009117+kimrasng@users.noreply.github.com>
2023-12-02 17:27:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const http = require('http');  // Changed from https to http
 
-const https = require("https");
 const fs = require('fs');
 
 const app = express();
 const PORT = 4000;
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './config/key.pem')),  // Replace with your Cloudflare private key path
-  cert: fs.readFileSync(path.join(__dirname, './config/cert.pem')),  // Replace with your Cloudflare certificate path
-};
-
 app.use(cors());
 
-// 정적 파일 제공
+// Static file serving
 
-// /api/music-server/music 로 접속할 경우 /music 폴더를 제공한다.
 app.use('/api/music-server/music', express.static(path.join(__dirname, '/music')));
-
-// /api/music-server/img/song 로 접속할 경우 /img/song 폴더를 제공한다.
 app.use('/api/music-server/img/song', express.static(path.join(__dirname, '/img/song')));
-
-// /api/music-server/img/artist 로 접속할 경우 /img/artist 폴더를 제공한다.
 app.use('/api/music-server/img/artist', express.static(path.join(__dirname, '/img/artist')));
-
-// /song/ 로 접속할 경우 /page/song 폴더를 제공한다.
 app.use('/song/', express.static(path.join(__dirname, '/page/song')));
 
-// main page
+// Main page
 app.get('/', (req, res) => {
-  const songsPath = path.join(__dirname, './page/main.html');
+  const mainPagePath = path.join(__dirname, './page/main.html');
 
-  fs.readFile(songsPath, 'utf8', (err, data) => {
+  fs.readFile(mainPagePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Internal Server Error', details: err.message });
@@ -44,11 +32,11 @@ app.get('/', (req, res) => {
   });
 });
 
-// SONG PAGE
+// Song page
 app.get('/song', (req, res) => {
-  const songsPath = path.join(__dirname, './page/song.html');
+  const songPagePath = path.join(__dirname, './page/song.html');
 
-  fs.readFile(songsPath, 'utf8', (err, data) => {
+  fs.readFile(songPagePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Internal Server Error', details: err.message });
@@ -59,7 +47,8 @@ app.get('/song', (req, res) => {
   });
 });
 
-// API: 곡 목록 및 정보 제공
+// API routes
+
 app.get('/api/music-server/songs', (req, res) => {
   const songsPath = path.join(__dirname, './songs.json');
 
@@ -90,7 +79,7 @@ app.get('/api/music-server/artist', (req, res) => {
   });
 });
 
-// 서버 시작
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`HTTPS server started on port ${PORT}`);
+// HTTP server start
+http.createServer(app).listen(PORT, () => {
+  console.log(`HTTP server started on port ${PORT}`);
 });
